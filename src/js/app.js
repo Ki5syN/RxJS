7bfdf736af03764a81bcfc6e1afc2e6b4108b87e
Bazinga <ki5syn333@gmail.com>
2026-09-04 13:58:30 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+import { ajax } from 'rxjs/ajax';
+import { exhaustMap, catchError } from 'rxjs/operators';
+import { interval, of } from 'rxjs'; // Добавили импорт 'of'
+import render from './messageWidget';
+
+const stream$ = interval(4000).pipe(
+  exhaustMap(() => {    
+    return ajax.getJSON('http://localhost:7071/messages/unread').pipe(
+      catchError(err => {        
+        console.error('Ошибка бэкенда:', err);
+        return of({ messages: [] });
+      })
+    );
+  })
+);
+
+stream$.subscribe(response => {
+  if (response && response.messages) {
+    render(response.messages);
+  }
+})
---
+++ src/js/messageWidget.js
@@ -0,0 +1,41 @@
+
+const list = document.querySelector('.list');
+
+
+function renderMessage(data){
+  const {from, subject, received } = data
+  const dateObject = new Date(received);
+  let subjetText = subject.length > 15 ? subject.slice(0, 15) + '...': subject;
+
+  let item = document.createElement('li')
+
+  let emailFrom = document.createElement('span');
+  emailFrom.classList.add('item-email');
+  emailFrom.textContent = from;
+
+  let textMessage = document.createElement('span');
+  textMessage.classList.add('item-textl');
+  textMessage.textContent = subjetText;
+
+  let dateMessage = document.createElement('time');
+  dateMessage.classList.add('item-date');
+  dateMessage.textContent = dateObject.toLocaleString('ru-RU', {
+      hour: '2-digit',
+      minute: '2-digit',
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    }); 
+
+  item.append(emailFrom, textMessage, dateMessage);
+  list.prepend(item);
+
+}
+
+export default function render(arr) {
+
+  let arrMessage = arr.forEach(el => {
+    renderMessage(el)    
+  });
+
+}
